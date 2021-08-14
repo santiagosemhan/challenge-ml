bcec1efd3782e2d852d314e4bbbed9b90d443338
Santiago Semhan <santiagosemhan@gmail.com>
2021-08-14 10:17:53 -0300

    Add Search component

---
+++ components/SearchBox/SearchBox.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import searchIcon from '../../public/static/img/search.png';
+import { useRouter } from 'next/dist/client/router';
+
+type Props = {
+  query?: string | string[];
+};
+
+const SearchBox: React.FC<Props> = ({ query }: Props) => {
+  const router = useRouter();
+  const [criteria, setCriteria] = useState(query || '');
+  const handleSubmit = React.useCallback(
+    (e) => {
+      e.preventDefault();
+      router.push(`/items?search=${criteria}`);
+    },
+    [router, criteria]
+  );
+
+  return (
+    <div className="flex flex-grow w-96">
+      <form
+        method="GET"
+        role="search"
+        className="bg-white flex w-4/6 shadow"
+        onSubmit={handleSubmit}
+      >
+        <input
+          className="w-full px-4 py-2"
+          type="text"
+          value={criteria}
+          onChange={(e) => setCriteria(e.currentTarget.value)}
+          placeholder="Buscar productos marcas y más..."
+        />
+        <button
+          type="submit"
+          tabIndex={0}
+          className="flex items-center w-7 pr-2"
+        >
+          <Image src={searchIcon} />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBox;
