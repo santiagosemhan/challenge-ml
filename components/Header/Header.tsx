a58b3e72002db676006ea0039bca0ab2dc14bfa7
Santiago Semhan <santiagosemhan@gmail.com>
2021-08-16 11:22:48 -0300

    Fix main logo link

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -12,7 +12,9 @@ const Header = () => {
       <div className="container mx-auto px-24 py-3 flex items-center space-x-12">
         <div className="w-36 flex items-center cursor-pointer">
           <Link href="/">
-            <Image src={logo} layout="intrinsic" />
+            <a>
+              <Image src={logo} layout="intrinsic" />
+            </a>
           </Link>
         </div>
         <SearchBox query={query.search} />
