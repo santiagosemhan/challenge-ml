7363e1574a86a6440e1870c14d247f4062eac0d4
Santiago Semhan <santiagosemhan@gmail.com>
2021-08-16 13:37:32 -0300

    Remove react-query dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,15 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import Layout from '@/components/Layout/Layout';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import '@/styles/global.css';
 
-const queryClient = new QueryClient();
+import '@/styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
+    <Layout>
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
