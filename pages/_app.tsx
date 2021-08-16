682bc321383944a89cd9b834954182f828fae4d0
Santiago Semhan <santiagosemhan@gmail.com>
2021-08-16 14:38:36 -0300

    Add progress bar on client transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout/Layout';
+import NextNProgress from 'nextjs-progressbar';
 
 import '@/styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <NextNProgress
+        color="#2c3479"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
     </Layout>
