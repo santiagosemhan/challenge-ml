c22e94c1fb5386f5aeffaa6ea1b86904b2721cc9
Santiago Semhan <santiagosemhan@gmail.com>
2021-08-16 11:24:23 -0300

    Index Page initial approach

---
+++ components/ProductRow/ProductRow.tsx
@@ -0,0 +1,65 @@
+import Image from 'next/image';
+import React from 'react';
+import { displayPrice } from '../../utils/';
+import shippingLogo from '../../public/static/img/free_shipping.png';
+import Link from 'next/link';
+
+type Props = {
+  product: {
+    id: string;
+    title: string;
+    price: {
+      amount: number;
+      currency: string;
+      decimals: number;
+    };
+    picture: string;
+    location: string;
+    free_shipping: boolean;
+    state_name: string;
+  };
+};
+
+const ProductRow: React.FC<Props> = ({ product }: Props) => {
+  return (
+    <div className="flex">
+      <div className="w-48 cursor-pointer">
+        <Link href={`/items/${product.id}`}>
+          <a>
+            <Image
+              src={product.picture}
+              width={170}
+              height={170}
+              layout="intrinsic"
+              objectFit="contain"
+              quality={99}
+              alt={product.title}
+            />
+          </a>
+        </Link>
+      </div>
+      <div className="flex flex-col flex-grow space-y-2 py-4">
+        <div className="flex items-center space-x-2">
+          <Link href={`/items/${product.id}`}>
+            <span className="text-2xl font-light cursor-pointer">
+              $ {displayPrice(product.price)}
+            </span>
+          </Link>
+          {product.free_shipping && (
+            <span className="text-xs w-5 h-5">
+              <Image src={shippingLogo} quality={90} alt="Free shipping" />
+            </span>
+          )}
+        </div>
+        <span className="text-lg text-gray-900 w-5/6 font-light">
+          {product.title}
+        </span>
+      </div>
+      <div className="w-36">
+        <span className="text-sm text-gray-500">{product.state_name}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProductRow;
---
+++ pages/items/index.tsx
@@ -0,0 +1,55 @@
+import Head from 'next/head';
+import { GetServerSideProps, NextPage } from 'next';
+import ProductRow from '@/components/ProductRow/ProductRow';
+
+type Props = {
+  search: string;
+  items: any[];
+};
+
+export const Items: NextPage<Props> = ({
+  search,
+  items,
+}: Props): JSX.Element => (
+  <div className="container mx-auto px-24 py-8">
+    <Head>
+      <title>Mercado Libre Argentina</title>
+    </Head>
+    {items.length > 0 ? (
+      <div className="bg-white p-4">
+        {items.map((product) => (
+          <ProductRow key={product.id} product={product} />
+        ))}
+      </div>
+    ) : (
+      <div className="flex justify-center py-12">{`No se han encontrado resultados para su búsqueda "${search}"`}</div>
+    )}
+  </div>
+);
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { req, query } = context;
+
+  if (!query.search) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  const response = await fetch(
+    `http://${req.headers.host}/api/items?q=${query.search}`
+  );
+  const results = await response.json();
+
+  return {
+    props: {
+      search: query.search || null,
+      items: results.items,
+    },
+  };
+};
+
+export default Items;
